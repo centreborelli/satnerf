3387045c8609c87b5d2ceafaf93b0b3cee5d87a0
rogermm14 <rogermarimolas@gmail.com>
2022-02-03 13:18:17 +0100

    +

--- run_exp_cvprw1.sh
+++ run_exp_cvprw1.sh
@@ -41,7 +41,7 @@ exp_name="$aoi_id"_"$imsize"_satnerf_beta
 python3 main.py --root_dir $satnerf_data_dir/"$aoi_id"_ba_v2 --img_dir $DFC2019_dir/Track3-RGB/$aoi_id --config_name s-nerf-w --cache_dir $satnerf_data_dir/"$aoi_id"_ba_v2/cache_$imsize --img_downscale $downsample_factor --exp_name "$exp_name"_"$suffix" --checkpoints_dir $out_dir/ckpts_cvprw_2 --logs_dir $out_dir/logs_cvprw_2 --gt_dir $DFC2019_dir/Track3-Truth --gpu_id $gpu_id --solarloss_lambda 0 --uncertainty 2>> $errs
 
 # shadow-NeRF + solar correction
-exp_name="$aoi_id"_"$imsize"_satnerf_beta_SCx0.05
+exp_name="$aoi_id"_"$imsize"_snerf_beta_SCx0.05
 python3 main.py --root_dir $satnerf_data_dir/"$aoi_id"_ba_v2 --img_dir $DFC2019_dir/Track3-RGB/$aoi_id --config_name s-nerf --cache_dir $satnerf_data_dir/"$aoi_id"_ba_v2/cache_$imsize --img_downscale $downsample_factor --exp_name "$exp_name"_"$suffix" --checkpoints_dir $out_dir/ckpts_cvprw_2 --logs_dir $out_dir/logs_cvprw_2 --gt_dir $DFC2019_dir/Track3-Truth --gpu_id $gpu_id --solarloss_lambda 0.05 2>> $errs
 
 

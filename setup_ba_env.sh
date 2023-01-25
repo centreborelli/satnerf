9975508474380728af3d72f00e1b0f34a6c35566
David Ramirez <dframirez.usmc@gmail.com>
2023-01-25 15:22:35 -0700

    Ensure setuptools version <=58
    Resolves issue with setuptools loss of support for "use_2to3".
    Resolves "pip install sat-bundleadjust" issue "error in ad setup command: use_2to3 is invalid."

--- setup_ba_env.sh
+++ setup_ba_env.sh
@@ -1,6 +1,7 @@
 ### create satnerf venv
 conda create -n ba -c conda-forge python=3.8
 conda activate ba
+python3 -m pip install --force-reinstall -v "setuptools<=58"
 python3 -m pip install git+https://github.com/centreborelli/sat-bundleadjust
 pip install fire
 #pip install "pyproj>=2.0.2,<3.0.0"

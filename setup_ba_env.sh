ebb8c372426a1275d6a9a660f96cadea83f6dc1d
David Ramirez <dframirez.usmc@gmail.com>
2023-01-25 17:05:17 -0700

    Include libgdal and torch into dependencies
    Needed for special execution

--- setup_ba_env.sh
+++ setup_ba_env.sh
@@ -1,9 +1,10 @@
 ### create satnerf venv
-conda create -n ba -c conda-forge python=3.8
+conda create -n ba -c conda-forge python=3.8 libgdal
 conda activate ba
-python3 -m pip install --force-reinstall -v "setuptools<=58"
-python3 -m pip install git+https://github.com/centreborelli/sat-bundleadjust
+python3 -m pip install --ignore-installed certifi gdal
+python3 -m pip install --force-reinstall -v "setuptools<=58" pip
+python3 -m pip install --ignore-installed git+https://github.com/centreborelli/sat-bundleadjust
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 -f https://download.pytorch.org/whl/torch_stable.html
 pip install fire
-#pip install "pyproj>=2.0.2,<3.0.0"
 conda deactivate
 echo "ba conda env created !"
